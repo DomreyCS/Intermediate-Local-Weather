1e6676e5eeec4073c11ab06caf0ba8eadffce1f5
Dara Hoy <darahoy@gmail.com>
2018-05-23 20:17:28 +0000

    Clean syntax & markup, add comments

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const geo = navigator.geolocation;
-
 geo.getCurrentPosition((position) => {
 	weather.fetch('GET', `https://fcc-weather-api.glitch.me/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
 		.then(data => {
 			weather.show(JSON.parse(data));
 		});
 });
-
+// Event listeners for unit conversion click
+document.getElementById('unit')
+	.addEventListener('click', function() {
+		weather.convert();
+	});
 let weather = {
 	fetch(method, url) {
 		return new Promise((resolve, reject) => {
@@ -33,26 +36,35 @@ let weather = {
 	},
 	show(response) {
 		let data = response;
-		document.getElementById('clouds').innerHTML = response.clouds.all; 
-		document.getElementById('winds').innerHTML = response.wind.speed; 
-		document.getElementById('pop').innerHTML = response.main.humidity;
-		document.getElementById('temperature').innerHTML = Math.round(response.main.temp);
-		document.getElementById('max').innerHTML = response.main.temp_max;
-		document.getElementById('min').innerHTML = response.main.temp_min;
-		document.getElementById('city').innerHTML = response.name;
-		document.getElementById('country').innerHTML = response.sys.country;
-		document.getElementById('desc').innerHTML = response.weather[0].main;
-		document.getElementById('icon').innerHTML = `<img src="${response.weather["0"].icon}" style="height: 150px;width: 175px;"></img>`;
+		document.getElementById('clouds')
+			.innerHTML = response.clouds.all;
+		document.getElementById('winds')
+			.innerHTML = response.wind.speed;
+		document.getElementById('pop')
+			.innerHTML = response.main.humidity;
+		document.getElementById('temperature')
+			.innerHTML = Math.round(response.main.temp);
+		document.getElementById('max')
+			.innerHTML = response.main.temp_max;
+		document.getElementById('min')
+			.innerHTML = response.main.temp_min;
+		document.getElementById('city')
+			.innerHTML = response.name;
+		document.getElementById('country')
+			.innerHTML = response.sys.country;
+		document.getElementById('desc')
+			.innerHTML = response.weather[0].main;
+		document.getElementById('icon')
+			.innerHTML = `<img src="${response.weather["0"].icon}" style="height: 150px;width: 175px;"></img>`;
 	},
 	convert() {
 		let unit = document.getElementById('unit');
 		let temp = document.getElementById('temperature');
 		let max = document.getElementById('max');
 		let min = document.getElementById('min');
-		
 		let temps = [temp, min, max];
 		console.log(temps);
-		switch(unit.innerHTML) {
+		switch (unit.innerHTML) {
 			case '℉':
 				temps.map(element => {
 					unit.innerText = '℃';
@@ -68,9 +80,3 @@ let weather = {
 		}
 	}
 }
-
-document.getElementById('unit').addEventListener('click', function() {
-	weather.convert();
-});
-
-// 	url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime(), true); //prevents caching API response by adding new Date object to params
