a8a3c38094c9a1f710e099727eabd40919a01aa8
Dara Hoy <darahoy@gmail.com>
2018-05-23 19:35:44 +0000

    Add temperature conversion

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const geo = navigator.geolocation;
 
 geo.getCurrentPosition((position) => {
 	weather.fetch('GET', `https://fcc-weather-api.glitch.me/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
-		.then(result => {
-			console.log(result);
+		.then(data => {
+			weather.show(JSON.parse(data));
 		});
 });
 
@@ -31,9 +31,46 @@ let weather = {
 			xhr.send();
 		})
 	},
-	show(unit) {
-	    // conversion based on unit
-	    // DOM manipulations
+	show(response) {
+		let data = response;
+		document.getElementById('clouds').innerHTML = response.clouds.all; 
+		document.getElementById('winds').innerHTML = response.wind.speed; 
+		document.getElementById('pop').innerHTML = response.main.humidity;
+		document.getElementById('temperature').innerHTML = Math.round(response.main.temp);
+		document.getElementById('max').innerHTML = response.main.temp_max;
+		document.getElementById('min').innerHTML = response.main.temp_min;
+		document.getElementById('city').innerHTML = response.name;
+		document.getElementById('country').innerHTML = response.sys.country;
+		document.getElementById('desc').innerHTML = response.weather[0].main;
+		document.getElementById('icon').innerHTML = `<img src="${response.weather["0"].icon}" style="height: 150px;width: 175px;"></img>`;
+	},
+	convert() {
+		let unit = document.getElementById('unit');
+		let temp = document.getElementById('temperature');
+		let max = document.getElementById('max');
+		let min = document.getElementById('min');
+		
+		let temps = [temp, min, max];
+		console.log(temps);
+		switch(unit.innerHTML) {
+			case '℉':
+				temps.map(element => {
+					unit.innerText = '℃';
+					element.innerHTML = Math.round((element.innerHTML - 32) * 5 / 9)
+				});
+				break;
+			case '℃':
+				temps.map(element => {
+					unit.innerText = '℉';
+					element.innerHTML = Math.round(element.innerHTML * 9 / 5 + 32);
+				});
+				break;
+		}
 	}
 }
+
+document.getElementById('unit').addEventListener('click', function() {
+	weather.convert();
+});
+
 // 	url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime(), true); //prevents caching API response by adding new Date object to params
